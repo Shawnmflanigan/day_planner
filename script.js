c9f41238eb5c890a65bdefbfd8f46ef67115cce3
Shawn Flanigan <shawnmflanigan@gmail.com>
2020-12-08 09:49:28 -0800

    Add top of screen date display

---
+++ script.js
@@ -0,0 +1,11 @@
+// write function to print current day at top of screen
+var today = moment ();
+var displayToday = document.getElementById('currentDay');
+displayToday.innerHTML = today.format('dddd, MMMM Do, YYYY');
+// write function to tell what time it is using moment.js
+    //if current time is past present or future change class to access css styling
+    //write function to allow input into data class
+    //write function for save button
+        //print text when save button is clicked
+        //store in local storage
+        //retrieve data when page is refreshed
