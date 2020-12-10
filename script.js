302bb7fd16847ed664fd2bd0105067e6319fa1d7
Shawn Flanigan <shawnmflanigan@gmail.com>
2020-12-10 08:27:41 -0800

    add time function, clean up html

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // write function to print current day at top of screen
-var today = moment ();
-var displayToday = document.getElementById('currentDay');
+
+let today = moment ();
+let displayToday = document.getElementById('currentDay');
 displayToday.innerHTML = today.format('dddd, MMMM Do, YYYY');
+
 // write function to tell what time it is using moment.js
+function compareTime() {
+    let currentTime = moment().hour();
+    $('.time-block').each(function(){
+        let newTime = $(this).children('.hour').attr('id');
+        if (newTime < currentTime) {
+            $(this).addClass('past');
+        }
+        else if (newTime == currentTime) {
+            $(this).addClass('present') 
+            $(this).removeClass('past')
+        }
+        else {
+            $(this).addClass('future')
+            $(this).removeClass('past')
+            $(this).removeClass('present');
+        }
+
+    }) 
+    console.log(currentTime)
+}
+compareTime ();
+setInterval (compareTime, 100000);
+
+    
     //if current time is past present or future change class to access css styling
     //write function to allow input into data class
     //write function for save button
